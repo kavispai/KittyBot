8a3890fa2b3399c1965941472b66c3e9b9957bc3
Kseniya <94558830+kavispai@users.noreply.github.com>
2022-05-14 19:51:44 +0300

    Add files via upload

---
+++ kittybot.py
@@ -0,0 +1,74 @@
+import logging
+import requests
+import os
+
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+from telegram import ReplyKeyboardMarkup
+
+from dotenv import load_dotenv
+
+load_dotenv()
+auth_token = os.getenv('TOKEN')
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
+URL = 'https://api.thecatapi.com/v1/images/search'
+new_url = 'https://api.thedogapi.com/v1/images/search'
+
+
+def get_new_image():
+    try:
+        response = requests.get(URL)
+    except Exception as error:
+        logging.error(f'Ошибка при запросе к основному API: {error}')
+        response = requests.get(new_url)
+
+    response = response.json()
+    random_cat = response[0].get('url')
+    return random_cat
+
+
+def new_cat(update, context):
+    chat = update.effective_chat
+    context.bot.send_photo(chat.id, get_new_image())
+
+
+def say_hi(update, context):
+    chat = update.effective_chat
+    name = update.message.chat.first_name
+    context.bot.send_message(
+        chat_id=chat.id,
+        text='Привет, {}! я KittyBot!'.format(name)
+    )
+
+
+def wake_up(update, context):
+    chat = update.effective_chat
+    name = update.message.chat.first_name
+    # За счёт параметра resize_keyboard=True кнопка поменьше
+    button = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
+
+    context.bot.send_message(
+        chat_id=chat.id,
+        text='Привет, {}. Посмотри, какого котика я тебе нашёл'.format(name),
+        reply_markup=button
+    )
+
+    context.bot.send_photo(chat.id, get_new_image())
+
+
+def main():
+    updater = Updater(token=auth_token)
+
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
